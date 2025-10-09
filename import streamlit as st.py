6da22e5760cb65d43a8e23a5a9383f2835f19018
andrescruz7777-arch <andrescruz7777@gmail.com>
2025-10-09 12:00:51 -0500

    Update import streamlit as st.py

--- import streamlit as st.py
+++ import streamlit as st.py
@@ -97,7 +97,7 @@ Estoy aquí para brindarte información de tus productos y opciones de negociaci
 # ============================
 st.markdown("<br>", unsafe_allow_html=True)
 col1, col2, col3 = st.columns([1, 2, 1])
-with col2.5:
+with col2:
     start = st.button("🚀 INICIAR CHATBOT")
 
 # Si el usuario inicia, creamos el estado
